39cc8f004ec037081e24abfa629eb18957a348b3
FantasySilence <1735793009@qq.com>
2024-03-13 23:40:21 +0800

    nothing update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 -----------------------------------------
 @Author: Fantasy_Silence
 @Time: 2024/3/11
-@IDE: PyCharm Python: 3.9.7
+@IDE: PyCharm&Visual Studio Code Python: 3.9.7
 -----------------------------------------
 这里是主程序入口
 """
